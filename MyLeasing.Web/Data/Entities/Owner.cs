a85776c22c29a9096dec831ba1f05745f9c61dde
Gabriel Eduardo Acosta Nuñez <gacosta.com.do@outlook.com>
2019-08-11 21:56:41 -0400

    Add project files.

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+
+    }
+}
